5a4166628fbe0f99dade9f1a17932781567f4da6
Raghunandan Krishnamurthy <raghunandan13k@gmail.com>
2014-12-03 15:42:23 +0530

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,14 @@
+function string getProfileImage(lookupId) {
+  var map = 
+  {
+    a:b
+  };
+  
+  if(typeof map[lookupId] != "undefined") 
+  {
+    return map[lookupId];
+  }
+  
+  // return default image
+  return 'https://cloud.githubusercontent.com/assets/10024457/5278526/5f1952a6-7b00-11e4-97d4-88f1c15edca5.png';
+}
