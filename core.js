3e734201b455f8c8ef7c526c78d45bb32c05b9d7
Raghunandan Krishnamurthy <raghunandan13k@gmail.com>
2014-12-03 13:06:13 +0530

    Update core.js

--- core.js
+++ core.js
@@ -11,45 +11,47 @@
 		var data = new google.visualization.DataTable();
 
 		data.addColumn('string', 'CHILD');
-        data.addColumn('string', 'FATHER');
-        data.addColumn('string', 'ALIVE');	
+        	data.addColumn('string', 'FATHER');
+		data.addColumn('string', 'ALIVE');	
 		data.addColumn('string', 'MOBILE');		
 		data.addColumn('string', 'WHATSAPP');
+		data.addColumn('string', 'EMAIL');
+		data.addColumn('string', 'OTHER');
 		
         data.addRows([
-		[{v:'1', f:'KOLIWADA'}, '1', 'DEAD', '', '' ],
-		  [{v:'1001', f:'VEDANARAYANA BHAT'}, '1', 'DEAD', '', '' ],          			
-          [{v:'2001', f:'MADHAVA BHAT'}, '1001', 'ALIVE', '', '' ],
-			[{v:'20011', f:'SATHISH BHAT'}, '2001', 'ALIVE', '', '' ],
-			[{v:'20012', f:'MAHESH BHAT'}, '2001', 'ALIVE', '', '' ],
-				[{v:'200121', f:'PANNAGA BHAT'}, '20012', 'ALIVE', '', '' ],
-			[{v:'20013', f:'HARISH BHAT'}, '2001', 'ALIVE', '', '' ],
-          [{v:'2002', f:'GANGHADHARA BHAT'}, '1001', 'ALIVE', '', '' ],
-			[{v:'20021', f:'BALACHANDRA K G'}, '2002', 'ALIVE', '', '' ],
-				[{v:'200211', f:'GOUTHAM BHAT'}, '20021', 'ALIVE', '', '' ],
-				[{v:'200212', f:'GOVRAW BHAT'}, '20021', 'ALIVE', '', '' ],
-			[{v:'20022', f:'RAGHAVENDRA K G'}, '2002', 'ALIVE', '', '' ],
-				[{v:'200221', f:'SUHRUTH SHANKARA BHAT'}, '20022', 'ALIVE', '', '' ],
-          [{v:'2003', f:'RANGANATHA BHAT'}, '1001', 'ALIVE', '', '' ],
-			[{v:'20031', f:'KIRAN KUMAR R'}, '2003', 'ALIVE', '', '' ],
-				[{v:'200311', f:'PRATHAM'}, '20031', 'ALIVE', '', '' ],
-          [{v:'2004', f:'SUNDARESH BHAT'}, '1001', 'ALIVE', '', '' ],
-			[{v:'20041', f:'DEEPAK'}, '2004', 'ALIVE', '', '' ],
-		  [{v:'2005', f:'KESHAVA MURTHY'}, '1001', 'ALIVE', '', '' ],
-			[{v:'20051', f:'ANIL BHAT'}, '2005', 'ALIVE', '', '' ],
-				[{v:'200511', f:'ABHINAV BHAT'}, '20051', 'ALIVE', '', '' ],
-			[{v:'20052', f:'SUNIL MURTHY'}, '2005', 'ALIVE', '', '' ],
-				[{v:'200521', f:'ROHAN MURTHY'}, '20052', 'ALIVE', '', '' ],
-          [{v:'2006', f:'KRISHNAMURTHY'}, '1001', 'ALIVE', '', '' ],
-			[{v:'20061', f:'RAGHUNANDAN KRISHNAMURTHY'}, '2006', 'ALIVE', '9900166130', 'YES' ],
-				[{v:'200611', f:'SAMARTH RAGHUNANDAN'}, '20061', 'ALIVE', '', '' ],
-			[{v:'20062', f:'VEDANARAYANA KRISHNAMURTHY'}, '2006', 'ALIVE', '', '' ],
-          [{v:'2007', f:'SURESH'}, '1001', 'ALIVE', '', '' ],
+			[{v:'1', f:'KOLIWADA'}, '1', 'DEAD', '', '', '', '' ],
+		  [{v:'1001', f:'VEDANARAYANA BHAT'}, '1', 'DEAD', '', '', '', '' ],          			
+          [{v:'2001', f:'MADHAVA BHAT'}, '1001', 'ALIVE', '', '', '', '' ],
+			[{v:'20011', f:'SATHISH BHAT'}, '2001', 'ALIVE', '', '', '', '' ],
+			[{v:'20012', f:'MAHESH BHAT'}, '2001', 'ALIVE', '', '', '', '' ],
+				[{v:'200121', f:'PANNAGA BHAT'}, '20012', 'ALIVE', '', '', '', '' ],
+			[{v:'20013', f:'HARISH BHAT'}, '2001', 'ALIVE', '', '', '', '' ],
+          [{v:'2002', f:'GANGHADHARA BHAT'}, '1001', 'ALIVE', '', '', '', '' ],
+			[{v:'20021', f:'BALACHANDRA K G'}, '2002', 'ALIVE', '', '', '', '' ],
+				[{v:'200211', f:'GOUTHAM BHAT'}, '20021', 'ALIVE', '', '', '', '' ],
+				[{v:'200212', f:'GOVRAW BHAT'}, '20021', 'ALIVE', '', '', '', '' ],
+			[{v:'20022', f:'RAGHAVENDRA K G'}, '2002', 'ALIVE', '', '', '', '' ],
+				[{v:'200221', f:'SUHRUTH SHANKARA BHAT'}, '20022', 'ALIVE', '', '', '', '' ],
+          [{v:'2003', f:'RANGANATHA BHAT'}, '1001', 'ALIVE', '', '', '', '' ],
+			[{v:'20031', f:'KIRAN KUMAR R'}, '2003', 'ALIVE', '', '', '', '' ],
+				[{v:'200311', f:'PRATHAM'}, '20031', 'ALIVE', '', '', '', '' ],
+          [{v:'2004', f:'SUNDARESH BHAT'}, '1001', 'ALIVE', '', '', '', '' ],
+			[{v:'20041', f:'DEEPAK'}, '2004', 'ALIVE', '', '', '', '' ],
+		  [{v:'2005', f:'KESHAVA MURTHY'}, '1001', 'ALIVE', '', '', '', '' ],
+			[{v:'20051', f:'ANIL BHAT'}, '2005', 'ALIVE', '', '', '', '' ],
+				[{v:'200511', f:'ABHINAV BHAT'}, '20051', 'ALIVE', '', '', '', '' ],
+			[{v:'20052', f:'SUNIL MURTHY'}, '2005', 'ALIVE', '', '', '', '' ],
+				[{v:'200521', f:'ROHAN MURTHY'}, '20052', 'ALIVE', '', '', '', '' ],
+          [{v:'2006', f:'KRISHNAMURTHY'}, '1001', 'ALIVE', '', '', '', '' ],
+			[{v:'20061', f:'RAGHUNANDAN KRISHNAMURTHY'}, '2006', 'ALIVE', '9900166130', 'YES', 'raghunandan13k@gmail.com', '' ],
+				[{v:'200611', f:'SAMARTH RAGHUNANDAN'}, '20061', 'ALIVE', '', '', '', '' ],
+			[{v:'20062', f:'VEDANARAYANA KRISHNAMURTHY'}, '2006', 'ALIVE', '', '', '', '' ],
+          [{v:'2007', f:'SURESH'}, '1001', 'ALIVE', '', '', '', '' ],
 		  
-		  [{v:'1002', f:'PANDURANGA BHAT'}, '1', 'DEAD', '', '' ],
-			[{v:'10021', f:'RATHNAKARA BHAT'}, '1002', 'ALIVE', '', '' ],
-				[{v:'100211', f:'DILIP BHAT'}, '10021', 'ALIVE', '', '' ],
-					[{v:'1002111', f:'SHREYAS BHAT'}, '100211', 'ALIVE', '', '' ],
+		  [{v:'1002', f:'PANDURANGA BHAT'}, '1', 'DEAD', '', '', '', '' ],
+			[{v:'10021', f:'RATHNAKARA BHAT'}, '1002', 'ALIVE', '', '', '', '' ],
+				[{v:'100211', f:'DILIP BHAT'}, '10021', 'ALIVE', '', '', '', '' ],
+					[{v:'1002111', f:'SHREYAS BHAT'}, '100211', 'ALIVE', '', '', '', '' ],
         ]);
 		
         var chart = new google.visualization.OrgChart(document.getElementById('chart_div'));
