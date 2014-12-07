bfd1af8c5cf46c78185027dac19d3aa2c249bfa7
Raghunandan Krishnamurthy <raghunandan13k@gmail.com>
2014-12-07 11:38:24 +0530

    Create utility.js

---
+++ utility.js
@@ -0,0 +1,27 @@
+  function getImgData(chartContainer) {
+    var chartArea = chartContainer.getElementsByTagName('svg')[0].parentNode;
+    var svg = chartArea.innerHTML;
+    var doc = chartContainer.ownerDocument;
+    var canvas = doc.createElement('canvas');
+    canvas.setAttribute('width', chartArea.offsetWidth);
+    canvas.setAttribute('height', chartArea.offsetHeight);
+        
+        
+    canvas.setAttribute(
+        'style',
+        'position: absolute; ' +
+        'top: ' + (-chartArea.offsetHeight * 2) + 'px;' +
+        'left: ' + (-chartArea.offsetWidth * 2) + 'px;');
+    doc.body.appendChild(canvas);
+    canvg(canvas, svg);
+    var imgData = canvas.toDataURL('image/png');
+    canvas.parentNode.removeChild(canvas);
+    return imgData;
+  }
+     
+  function saveAsImg(chartContainer) {
+    var imgData = getImgData(chartContainer);
+    // Replacing the mime-type will force the browser to trigger a download
+    // rather than displaying the image in the browser window.
+    window.location = imgData.replace('image/png', 'image/octet-stream');
+  }
